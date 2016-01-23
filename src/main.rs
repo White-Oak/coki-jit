1e1632f476fa00fe2a49e190a5b6622b2212900e
White-Oak <lpzhelud@gmail.com>
2016-01-23 21:22:13 +0300

    Added getopts

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 extern crate regex;
 extern crate peruse;
 extern crate jitter;
+extern crate getopts;
 
 use grammar::*;
 use parser::program;
@@ -17,6 +18,7 @@ use ast_optimizer::optimize_ast;
 use std::fs::File;
 use std::env::args;
 use std::io::Read;
+use getopts::Options;
 
 
 pub mod lexer;
@@ -30,18 +32,37 @@ pub mod optimizer;
 pub mod ast_optimizer;
 
 
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+}
 fn main() {
-    let args: Vec<_> = args().collect();
+    let args: Vec<String> = args().collect();
+    let program = args[0].clone();
 
-    let max_optimizations = if args.len() > 2 {
-        args[2].parse::<u8>().unwrap()
-    } else {
-        100
+    let mut opts = Options::new();
+    opts.optopt("", "max-optimizations", "set a maximum number of optimization passes", "NUMBER");
+    opts.optflagopt("", "bin", "outputs a binary executable", "FILE");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
     };
-    let ref file = if args.len() > 2 {
-        &args[1] as &str
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    let file = if !matches.free.is_empty() {
+        &matches.free[0] as &str
     } else {
         "examples/oka.coki"
+    };
+
+    let max_optimizations = if matches.opt_present("max-optimizations") {
+        matches.opt_str("max-optimizations").unwrap().parse::<u8>().unwrap()
+    } else {
+        100
     };
 
     let mut contents = String::new();
